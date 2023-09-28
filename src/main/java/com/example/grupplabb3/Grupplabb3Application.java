11bd66d948def5cb15fd85c18bc9e574035c374b
Vicabi <115193194+Vicabi@users.noreply.github.com>
2023-09-28 09:24:59 +0200

    Initial commit

---
+++ src/main/java/com/example/grupplabb3/Grupplabb3Application.java
@@ -0,0 +1,13 @@
+package com.example.grupplabb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupplabb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupplabb3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grupplabb3/Grupplabb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupplabb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupplabb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
